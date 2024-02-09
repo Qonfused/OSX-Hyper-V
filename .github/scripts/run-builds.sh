b84f6217af59c805fe1d1f86f11dda73a3cf3496
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2024-02-09 17:14:11 -0600

    Update release workflow (testing)

--- .github/scripts/run-builds.sh
+++ .github/scripts/run-builds.sh
@@ -13,7 +13,7 @@ __PWD__=$(pwd); cd "$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../")"
 while read -r args; do
   while read -r BUILD; do
     # Set build type in config to target build version
-    REPLACE="$(sed "s/build: [A-Z]*/build: $BUILD/" src/build.yml)"
+    REPLACE="$(sed "s/^build: [A-Z]*/build: $BUILD/" src/build.yml)"
     echo "$REPLACE" > src/build.yml
     # Run build script
     TARGET="${args//|/-}"
@@ -21,6 +21,6 @@ while read -r args; do
     echo "Building \"EFI-$TAG-$TARGET-$BUILD.zip\"..."
     powershell.exe scripts/build.ps1 "$FLAGS"
     # Compress EFI directory
-    (cd dist && zip -r -X "../EFI-$TAG-$TARGET-$BUILD.zip" . >/dev/null)
+    tar.exe -czf "EFI-$TAG-$TARGET-$BUILD.zip" -C dist/EFI .
   done <<< $'RELEASE\nDEBUG'
 done <<< $'64-bit\nlegacy|64-bit\nlegacy|32-bit'
