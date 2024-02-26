9e6f8e079525da00f6c4a338a64e1bde1141a67d
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2024-02-26 08:47:43 -0600

    Update release build script

---
+++ .github/scripts/run-builds.ps1
@@ -0,0 +1,41 @@
+## @file
+# EFI build script for OSX Hyper-V
+#
+# Copyright (c) 2023, Cory Bennett. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+##
+
+# [CmdletBinding(PositionalBinding=$false)]
+# param (
+#   [string]$pwd = "$((Get-Item "$PSScriptRoot\..").FullName)",
+#   # Script arguments
+#   [parameter(ValueFromRemainingArguments)][string[]]$arguments
+# )
+
+$buildList = @('RELEASE', 'DEBUG')
+$flagsList = @(
+  @('--64-bit'),
+  @('legacy', '--32-bit'),
+  @('legacy', '--64-bit')
+)
+
+foreach ($build in $buildList) {
+  # Prepare for each build configuration
+  Remove-Item -Path dist -Recurse -ErrorAction SilentlyContinue
+  Remove-Item -Path src\build.lock -ErrorAction SilentlyContinue
+  (Get-Content src\build.yml) -replace '^build: [A-Z]*', "build: $build" |
+    Set-Content src\build.yml
+
+  # Run build for each flag configuration
+  foreach ($flags in $flagsList) {
+    # Run build script
+    $flags = $($flags -join ' ')
+    Write-Host "Starting $build build with flags: $flags"
+    powershell.exe "$pwd\scripts\build.ps1" $flags
+
+    # Compress EFI directory
+    $identifier = "$($flags -replace '--', '' -join '-')-$build"
+    mv src\build.lock dist\EFI\OC\build.lock
+    tar.exe -czf "EFI-$env:TAG-$identifier.zip" -C dist .
+  }
+}
