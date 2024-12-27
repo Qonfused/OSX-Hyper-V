e471c834bde52f10362898b5ce9319fd6b50e4e3
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2024-12-26 23:00:01 -0600

    Force X64 ISA for Gen2 VMs
    Since Generation 2 Hyper-V machines use 64-bit-only UEFI firmware and do not have CSM, IA32 versions of OpenCore will not work.
    
    Partially reverts d84213c58480bd2f75150f1a094203f2e7513c0a.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -31,13 +31,6 @@ $patches = @('-p config.yml')
 if (HasFlag '--legacy') { $patches += @('-p patch.legacy.yml') }
 if (HasFlag '--32-bit') {
   $patches += @('-p patch.32-bit.yml')
-  # Ensure the target architecture is set to IA32
-  (Get-Content "$pwd\src\build.yml") -replace 'target: X64', 'target: IA32' |
-   Set-Content "$pwd\src\build.yml"
-} else {
-  # Ensure the target architecture is set to X64
-  (Get-Content "$pwd\src\build.yml") -replace 'target: IA32', 'target: X64' |
-   Set-Content "$pwd\src\build.yml"
 }
 
 icm `
